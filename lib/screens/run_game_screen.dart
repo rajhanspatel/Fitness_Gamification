bf98854af7d22df92a53e402dd5f661bd71016fe
RAJHANS <rajhanspatel113@gmail.com>
2025-05-27 18:39:20 +0530

    update code

--- lib/screens/run_game_screen.dart
+++ lib/screens/run_game_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 
 class RunGameScreen extends StatelessWidget {
+  const RunGameScreen({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
